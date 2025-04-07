1edd8d1a775f536d932f5e49f9357dbfa12cc9b8
Ivan Milinkovic <ivanmcode@gmail.com>
2025-04-07 18:46:40 +0200

    Axios and tests

--- blogger-react/src/services/axios.ts
+++ blogger-react/src/services/axios.ts
@@ -4,4 +4,5 @@ import { API_URL, jsonHeaders } from "./ServiceCommon";
 export const authApiAxios = axios.create({
   baseURL: API_URL,
   headers: jsonHeaders,
+  adapter: "fetch", // tests are mocking the fetch api, so use fetch instead of the default xhr
 });
